005816f8a9fe8db29a771386c831ea7eadd2981d
‘sirelkhatim10’ <alxtaskssir@gmail.com>
2023-06-15 19:19:35 -0700

    gg

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry pori
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void){
+    puts("\"Programming is like building a multilingual puzzle"):
+	return (0):    
+}
